3efd66218a255f2a0364dd7c415dc40c0dd7b687
Xinran Che <gracexinran@gmail.com>
2019-04-09 23:05:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,8 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  var array.unshift(element);
+  array.unshift(element);
   return array;
 }
 
-function de
